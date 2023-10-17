c139c9eee47369791ca8f1246c6f49c6657800fb
Andy <klee682@aucklanduni.ac.nz>
2023-10-17 19:50:04 +1300

    changed dashboard to a better template

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 This repository is for a practice online banking software called FinSecure. Inspiration for the website is similar to westpac online banking website.
 
+
+Sources used in project:
+
+inspiration for website
+https://www.westpac.co.nz/personal/ways-to-bank/westpac-one-online-banking/
+
+dashboard template
+https://codepen.io/havardob/pen/ExvwGBr
+
+
+
+
+
 learning points in project:
--- src/main/java/com/bankingservice/bank/controller/html/DashboardController.java
+++ src/main/java/com/bankingservice/bank/controller/html/DashboardController.java
@@ -1,5 +1,14 @@
 package com.bankingservice.bank.controller.html;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class DashboardController {
-    
+
+    @PostMapping("/logout")
+    public String logout() {
+        // Perform any necessary logout actions (e.g., clearing user session).
+        return "home"; // Redirect to the "home.html" page.
+    }
 }
